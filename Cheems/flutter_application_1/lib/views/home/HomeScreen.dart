57642bada38e155c3c6a0dd4b5789bfc230f2633
TruongGiaThach <71328121+TruongGiaThach@users.noreply.github.com>
2021-06-06 21:12:53 +0700

    .

--- Cheems/flutter_application_1/lib/views/home/HomeScreen.dart
+++ Cheems/flutter_application_1/lib/views/home/HomeScreen.dart
@@ -12,9 +12,85 @@ class HomeScreen extends StatelessWidget {
     _user = user;
     _signInType = signInType;
   } //1 - gg sign in  2- fb sign in
-   
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  Container(
+                      alignment: Alignment.center,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(35),
+                        child: Image.network(
+                          _user.photoUrl,
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.scaleDown,
+                        ),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        _user.displayName,
+                        style: textFieldTextStyle(),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: GestureDetector(
+                      onTap: () {
+                        _authController.signOutGoogle();
+                      },
+                      child: new Icon(
+                        Icons.logout_outlined,
+                        size: 35,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: GestureDetector(
+          onTap: () => {
+            _showPicker(context),
+          },
+          child: CircleAvatar(
+            backgroundColor: Colors.cyan[100],
+            radius: 45,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(45),
+              child: Image.network(
+                _user.photoUrl,
+                height: 80,
+                width: 80,
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    /*
     return Scaffold(
         appBar: appBarMain(context) ,
         body: Column(
@@ -59,6 +135,6 @@ class HomeScreen extends StatelessWidget {
             )
           ],
         ),
-    );
+    );*/
   }
 }
