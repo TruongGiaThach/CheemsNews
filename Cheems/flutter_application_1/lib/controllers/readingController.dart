b824775a998cb2b6ba93f4e54bd0b24254fbe3af
TruongGiaThach <71328121+TruongGiaThach@users.noreply.github.com>
2021-06-25 23:32:22 +0700

    .

--- Cheems/flutter_application_1/lib/controllers/readingController.dart
+++ Cheems/flutter_application_1/lib/controllers/readingController.dart
@@ -1,19 +1,34 @@
-
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/Comment.dart';
 import 'package:flutter_application_1/models/News.dart';
 import 'package:flutter_application_1/services/FirestoreService.dart';
 import 'package:get/state_manager.dart';
 
 class ReadingController extends GetxController {
   News? news;
-  ReadingController(){news = null;}
+  ReadingController() {
+    news = null;
+  }
   var hasData = false.obs;
-  
+  var bottomIndex = 0.obs;
 
-  fletchNews(String id) async{
-    
+  fletchNews(String id) async {
     news = await FirestoreService.instance.getNewsById(id);
     if (news != null)
       hasData.value = true;
-    else hasData.value = false;
+    else
+      hasData.value = false;
+  }
+
+  addComment(Comments cmt) async {
+    if (news != null)
+      await FirestoreService.instance.addCmt(this.news!.id, cmt);
+  }
+
+  Future<List<Comments>> getListCmt() async {
+    List<Comments> tmp = [];
+    if (this.news != null)
+      tmp = await FirestoreService.instance.getAllCmt(this.news!.id);
+    return tmp;
   }
 }
