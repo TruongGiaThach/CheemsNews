4165df45b3175aea696e57a7856bf91302bf74ce
DangHaiThinh <19520976@gm.uit.edu.vn>
2021-07-03 14:09:19 +0700

    add grid to stock chart
    _add grid
    _fix title
    _add statistic to grid

--- Cheems/flutter_application_1/lib/services/httpServices.dart
+++ Cheems/flutter_application_1/lib/services/httpServices.dart
@@ -9,43 +9,28 @@ class HttpService {
     String apiKey = "c32nksaad3ieculvq3ng";
     String resolution = '';
     int timeRange = 0;
-    switch (timeWindow) {
-      case "1Day":
-        timeRange = 86400;
-        resolution = "5";
-        break;
-      case "1Week":
-        timeRange = 604800;
-        resolution = "30";
-        break;
-      case "1Month":
-        timeRange = 2592000;
-        resolution = "D";
-        break;
-      case "3Month":
-        timeRange = 7776000;
-        resolution = "D";
-        break;
-      case "1Year":
-        timeRange = 31536000;
-        resolution = "D";
-        break;
+    switch(timeWindow){
+      case "1Day": timeRange = 86400;
+      resolution = "30";
+      break;
+      case "1Week": timeRange = 604800;
+      resolution = "240";
+      break;
+      case "1Month": timeRange = 2592000;
+      resolution = "D";
+      break;
+      case "3Month": timeRange = 7776000;
+      resolution = "D";
+      break;
+      case "1Year": timeRange = 31536000;
+      resolution = "W";
+      break;
     }
     String timea, timeb;
     timeb = DateTime.now().toUtc().millisecondsSinceEpoch.toString();
     timeb = timeb.substring(0, timeb.length - 3);
     timea = (int.parse(timeb) - timeRange).toString();
-    final response = await get(Uri.parse(postsURL +
-        "symbol=" +
-        name +
-        "&&resolution=" +
-        resolution +
-        "&from=" +
-        timea +
-        "&to=" +
-        timeb +
-        "&token=" +
-        apiKey));
+    final response = await get(Uri.parse(postsURL + "symbol=" + name +"&&resolution=" + resolution + "&from=" + timea + "&to=" + timeb +"&token=" + apiKey));
 
     if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
